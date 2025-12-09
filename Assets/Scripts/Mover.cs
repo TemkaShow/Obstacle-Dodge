af888501dd94043d4ce9a0916e5654799e625cb6
Artem Hlyviak <artem.hlyvyak@gmail.com>
2025-12-09 23:21:44 +0200

    Moves and walls
    Add mover for player, cinemachine, basic collisions

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,7 +1,9 @@
+using System.Threading;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 10f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +13,10 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float valueX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float valueY = 0f;
+        float valueZ = Input.GetAxis("Vertical") * Time.deltaTime* moveSpeed;
+
+        transform.Translate(valueX, valueY, valueZ);
     }
 }
