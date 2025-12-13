432c0129b9a3e9ca3f98ed1cb3e988e94bbe21e8
Artem Hlyviak <artem.hlyvyak@gmail.com>
2025-12-13 16:53:11 +0200

    Add Methods
    Add 2 new methods for Debug and Mover logic

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,3 +1,4 @@
+//using System.Diagnostics;
 using System.Threading;
 using UnityEngine;
 
@@ -7,16 +8,26 @@ public class Mover : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        PrintInstruction();
     }
 
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void PrintInstruction() 
+    {
+        Debug.Log("Welcome to the game!");
+        Debug.Log("To move the player use WASD");
+    }
+
+    void MovePlayer() 
+    {
         float valueX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
         float valueY = 0f;
         float valueZ = Input.GetAxis("Vertical") * Time.deltaTime* moveSpeed;
-
         transform.Translate(valueX, valueY, valueZ);
     }
 }
